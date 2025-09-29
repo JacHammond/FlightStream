dea283a53bf2437f719153fcc65e4503b21428c8
Jacob Hammond <jacobmhammond@gmail.com>
2025-09-29 14:13:10 -0400

    Update Source.py

--- Source.py
+++ Source.py
@@ -4,9 +4,12 @@ from SimConnect import SimConnect, AircraftRequests
 from avwx import Metar
 import argparse
 
+# Default destination IP (receiver host)
+DestIp = "192.168.0.199"
+
 # Network config
-parser = argparse.ArgumentParser(description="FlightStream MSFS source (Windows)")
-parser.add_argument("--dest-ip", default=os.getenv("FLIGHT_UDP_DEST_IP", "127.0.0.1"),
+parser = argparse.ArgumentParser(description="FlightStream MSFS source (Running the Game)")
+parser.add_argument("--dest-ip", default=os.getenv("FLIGHT_UDP_DEST_IP", DestIp),
                     help="Destination IP (receiver host)")
 parser.add_argument("--dest-port", type=int, default=int(os.getenv("FLIGHT_UDP_DEST_PORT", "9000")),
                     help="Destination UDP port")
